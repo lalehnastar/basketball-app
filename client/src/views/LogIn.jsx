c7cb4f893965b6cb0783a3a17c07c94c8b0dbbd8
Laleh <laleh@Lilis-MacBook-Pro-2.local>
2018-04-09 12:11:32 -0700

    Login page with reactstrap form

--- client/src/views/LogIn.jsx
+++ client/src/views/LogIn.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import httpClient from '../httpClient'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class LogIn extends React.Component {
 	state = {
-		fields: { email: '', password: ''}
+		fields: { email: '', password: '' }
 	}
 
 	onInputChange(evt) {
 		this.setState({
+
 			fields: {
 				...this.state.fields,
 				[evt.target.name]: evt.target.value
@@ -15,32 +17,31 @@ class LogIn extends React.Component {
 		})
 	}
 
-	onFormSubmit(evt) {
+	onButtonClick(evt) {
 		evt.preventDefault()
 		httpClient.logIn(this.state.fields).then(user => {
 			this.setState({ fields: { email: '', password: '' } })
-			if(user) {
+			if (user) {
 				this.props.onLoginSuccess(user)
 				this.props.history.push('/')
 			}
 		})
 	}
-	
+
 	render() {
 		const { email, password } = this.state.fields
 		return (
-			<div className='LogIn'>
-				<div className='row'>
-					<div className='column column-33 column-offset-33'>
-						<h1>Log In</h1>
-						<form onChange={this.onInputChange.bind(this)} onSubmit={this.onFormSubmit.bind(this)}>
-							<input type="text" placeholder="Email" name="email" value={email} />
-							<input type="password" placeholder="Password" name="password" value={password} />
-							<button>Log In</button>
-						</form>
-					</div>
-				</div>
-			</div>
+			<Form>
+				<FormGroup>
+					<Label for="email">Email</Label>
+					<Input type="email" name="email" id="email" placeholder="Enter an Email" onChange={this.onInputChange.bind(this)} />
+				</FormGroup>
+				<FormGroup>
+					<Label for="password">Password</Label>
+					<Input type="password" name="password" id="password" placeholder="Enter password" onChange={this.onInputChange.bind(this)} />
+				</FormGroup>
+				<Button onClick={this.onButtonClick.bind(this)}>Submit</Button>
+			</Form>
 		)
 	}
 }
